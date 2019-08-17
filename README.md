56a7b99eb0087cdcf4de6bc019991bbe14384333
Johan Levin <johan13@gmail.com>
2019-08-17 17:27:41 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+Flight obstacles
+================
+
+This repository holds the code for https://map.joleco.se.
+
+Updating the list of obstacles
+------------------------------
+1. Find the URL to the current CSV file on lfv.se.  The URL changes frequently. Right now it can be found at
+   https://aro.lfv.se/Editorial/View/Dataset.
+2. Update the URL in download.ts
+3. Run `npm install && npm run download`.
+
+Deploying the app to AWS S3
+---------------------------
+Assuming AWS credentials are already configured in the joleco profile:
+```
+aws --profile=joleco s3 cp public s3://map.joleco.se/ --recursive
+```
---
+++ download.ts
@@ -0,0 +1,48 @@
+import { writeFileSync } from "fs";
+import fetch from "node-fetch";
+import * as path from "path";
+
+const OBSTACLE_CSV_URL = "https://aro.lfv.se/Editorial/View/6391/ES_ENR_5_4_WEF_20190815";
+
+main().catch(console.error);
+async function main() {
+    const response = await fetch(OBSTACLE_CSV_URL);
+    const csvText = (await response.buffer()).toString("latin1");
+    const obstacles = csvText
+        .split("\n")
+        .map(line => line.trimRight().split(";"))
+        .filter(cells => cells.length === 9 && cells[0] !== "NO")
+        .map(parseObstacle);
+    const outPath = path.resolve(__dirname, "public", "obstacles.json");
+    writeFileSync(outPath, JSON.stringify(obstacles));
+    console.log(`Wrote ${obstacles.length} objects to ${outPath}.`);
+}
+
+function parseObstacle([, name, lat, long, , heightFt, , , type]: string[]) {
+    const obstacle = {
+        name,
+        lat: parseCoordinate(lat),
+        long: parseCoordinate(long),
+        height: parseHeight(heightFt),
+        type,
+    };
+    return obstacle;
+}
+
+function parseCoordinate(str: string) {
+    const match = /^(\d{2,3})(\d{2})(\d{2}\.\d)[EN]$/.exec(str);
+    if (!match) {
+        throw new Error(`Failed to parse coordinate: ${JSON.stringify(str)}`);
+    }
+    const [, deg, min, sec] = match.map(parseFloat);
+    return Math.round((deg + min / 60 + sec / 3600) * 10000) / 10000;
+}
+
+function parseHeight(feet: string) {
+    const meters = parseFloat(feet) * 0.3048;
+    if (!Number.isFinite(meters)) {
+        throw new Error(`Failed to parse height: ${JSON.stringify(feet)}`);
+
+    }
+    return Math.round(meters);
+}
