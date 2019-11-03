823fb3dc90d4805307adaa5f9c956e4d091ceb79
Johan Levin <johan13@gmail.com>
2019-11-03 18:55:02 +0100

    Update to 20191205 data set plus some tweaks

--- download.ts
+++ download.ts
@@ -1,8 +1,8 @@
-import { writeFileSync } from "fs";
+import { promises as fs } from "fs";
 import fetch from "node-fetch";
 import * as path from "path";
 
-const OBSTACLE_CSV_URL = "https://aro.lfv.se/Editorial/View/6391/ES_ENR_5_4_WEF_20190815";
+const OBSTACLE_CSV_URL = "https://aro.lfv.se/Editorial/View/6819/ES_ENR_5_4_WEF_20191205";
 
 main().catch(console.error);
 async function main() {
@@ -19,11 +19,12 @@ async function main() {
             height: parseHeight(heightFt),
             type,
         }));
-    const outPath = path.resolve(__dirname, "public", "obstacles.json");
-    writeFileSync(outPath, JSON.stringify(obstacles));
+    const outPath = path.join(__dirname, "public", "obstacles.json");
+    await fs.writeFile(outPath, JSON.stringify(obstacles));
     console.log(`Wrote ${obstacles.length} objects to ${outPath}.`);
 }
 
+/** Parses a coordinate on the format used in the CSV file (deg, min, sec) into numeric degrees with four decimals. */
 function parseCoordinate(str: string) {
     const match = /^(\d{2,3})(\d{2})(\d{2}\.\d)[EN]$/.exec(str);
     if (!match) {
@@ -33,6 +34,7 @@ function parseCoordinate(str: string) {
     return Math.round((deg + min / 60 + sec / 3600) * 10000) / 10000;
 }
 
+/** Parses a textual height in feet into a numeric height in meters. */
 function parseHeight(feet: string) {
     const meters = parseFloat(feet) * 0.3048;
     if (!Number.isFinite(meters)) {
