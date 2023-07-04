a49c526761d00cecd4844ffa7a27379af2088443
Johan Levin <johan13@gmail.com>
2023-07-04 14:11:49 +0200

    Update obstacles list to 2023-08-10

--- download.ts
+++ download.ts
@@ -2,7 +2,7 @@ import fs from "node:fs/promises";
 import path from "node:path";
 import fetch from "node-fetch";
 
-const OBSTACLE_CSV_URL = "https://aro.lfv.se/Editorial/View/13057/ENR%205-4%20WEF_20230615";
+const OBSTACLE_CSV_URL = "https://aro.lfv.se/Editorial/View/13210/ENR%205-4%20WEF_20230810";
 
 main().catch(console.error);
 async function main() {
